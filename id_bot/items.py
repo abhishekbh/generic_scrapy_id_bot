8ffe157bd5eb8a962ec20f682965142047535778
Abhishek Bhatnagar <abhishekbh@gmail.com>
2016-07-26 00:05:18 +0000

    we're not using items

--- id_bot/items.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import scrapy
-
-class TutorialItem(scrapy.Item):
-    title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
--- id_bot/pipelines.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class TutorialPipeline(object):
-    def process_item(self, item, spider):
-        return item
